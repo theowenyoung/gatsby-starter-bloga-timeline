fa8e46fe617a6febf84b620c641b7cc73798f62e
Owen Young <theowenyoung@gmail.com>
2020-06-18 04:02:26 +0800

    feat: change options

--- utils/string.js
+++ utils/string.js
@@ -1,6 +1,7 @@
 const hashtag = require('hashtag');
 const _ = require("lodash");
-const HashtagParser = hashtag.HashtagParser;
+
+const {HashtagParser} = hashtag;
 exports.getTags = function (text) {
   const h = new HashtagParser(text, "#", "[\0-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]")
   return h.tags
@@ -9,7 +10,7 @@ exports.getTags = function (text) {
 
 exports.getTemplateValue = function (text, ctx) {
   _.templateSettings.interpolate = /\${{([\s\S]+?)}}/g;
-  var compiled = _.template(text);
+  const compiled = _.template(text);
   return compiled(ctx);
 
 }
