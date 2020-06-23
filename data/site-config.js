f172461cdcb5537eaada9466639cd116e032e6a8
Owen Young <theowenyoung@gmail.com>
2020-06-23 16:46:59 +0800

    feat: use remote config

--- data/site-config.js
+++ data/site-config.js
@@ -1,53 +1,16 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path')
-const _ = require('lodash')
-const requestSync = require('sync-request');
-const { getTemplateValue } = require('../utils/string')
 // Get document, or throw exception on error
-let defaultConfig; let userRemoteConfig; let userLocalConfig;
+let defaultConfig;
 try {
   defaultConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, './default-site-config.yaml'), 'utf8'));
 } catch (e) {
   console.error('load default yaml error', e);
 }
 
-// try go get remote bloga config 
-try {
-  // TODO add api key 
-  if (process.env.BLOGA_CONFIG) {
-    const res = requestSync('GET', process.env.BLOGA_CONFIG, {
-      socketTimeout: 10000,
-      timeout: 100000,
-    });
-    const blogaConfigResult = res.getBody('utf8');
-    userRemoteConfig = yaml.safeLoad(blogaConfigResult, 'utf8');
-    console.log('detect bloga remote config');
-  }
 
-} catch (e) {
-  console.error('There is no user config or load user config error, use default config', e);
-  process.exit()
-}
-
-// try go get local bloga config 
-try {
-
-  const localBlogaConfig = path.resolve(__dirname, '../bloga.yaml');
-
-  if (fs.existsSync(localBlogaConfig)) {
-
-    userLocalConfig = yaml.safeLoad(fs.readFileSync(localBlogaConfig, 'utf8'), 'utf8');
-    console.log('detect bloga local config');
-  }
-
-} catch (e) {
-  console.error('There is no user config or load user config error, use default config', e);
-  process.exit()
-  throw e;
-}
-
-const config = { ...defaultConfig, ...userRemoteConfig, ...userLocalConfig}
+const config = { ...defaultConfig}
 
 // Validate
 
@@ -66,40 +29,5 @@ if (config.siteUrl.substr(-1) === "/")
 // Make sure siteRss has a starting forward slash
 if (config.siteRss && config.siteRss[0] !== "/")
   config.siteRss = `/${config.siteRss}`;
-
-// parse the final value
-const iterate = (obj, keyPath) => {
-  Object.keys(obj).forEach(key => {
-    const value = obj[key]
-    if (typeof value === 'object' && value !== null) {
-      if (keyPath) {
-        iterate(obj[key], `${keyPath}[${key}]`)
-      } else {
-        iterate(obj[key], key)
-
-      }
-    } else {
-      // simple value
-      if (typeof value === 'string') {
-        // convert to template value
-
-        const newValue = getTemplateValue(value, {
-          env: process.env,
-          date: {
-            year: new Date().getFullYear(),
-          },
-          config
-        })
-        if (keyPath) {
-          _.set(config, `${keyPath}[${key}]`, newValue)
-        } else {
-          config[key] = newValue
-        }
-
-      }
-    }
-  })
-}
-iterate(config);
 
 module.exports = config;
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
-const gatsbyConfig = require('./_gatsby-config');
+require("dotenv").config()
+
+console.log('BLOGA_SITE_CONFIG_URL',process.env.BLOGA_SITE_CONFIG_URL)
+// before use this config, make sure you have already run `bloga-gatsby init` successfully.
+const {Config} = require('bloga-gatsby')
+
+const gatsbyConfig = new Config('./_gatsby.config').toConfig()
+console.log('finalConfig', JSON.stringify(gatsbyConfig, null, 2))
 
 module.exports = gatsbyConfig
